26979621ef98a52b0703399e586265ae0874fa53
wsu2059q <wsu2059@qq.com>
2025-07-07 22:38:31 +0800

    添加示例模块

---
+++ MyModule/Core.py
@@ -0,0 +1,30 @@
+
+class Main:
+    def __init__(self, sdk):
+        self.sdk = sdk
+        self.env = self.sdk.env
+        self.logger = self.sdk.logger
+
+        self.logger.info("MyModule 初始化中完成")
+        self.load_config()
+
+    def load_config(self):
+        self.config = self.env.loadConfig("MyModule")
+
+        if self.config is None:
+            self.logger.error("无法加载配置文件")
+
+            self.env.setConfig("MyModule", {
+                "key1": "value1",
+                "key2": ["value2", "value3"],
+                "key3": {
+                    "key4": "value4",
+                    "key5": "value5"
+                },
+                "key6": True
+            })
+            self.logger.error("已生成对应的配置内容到你的env.py中, 建议修改后再次运行")
+
+    def hello(self):
+        self.logger.info("Hello World!")
+        # 其它模块可以通过 sdk.MyModule.hello() 调用此方法
